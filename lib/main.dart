9059b3126d2f29e1dbbd14e2a99ccd1ab37a62db
taehyun <xogus5070318@gmail.com>
2024-07-13 00:39:59 +0900

    메뉴선택 프로토타입 구현

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class MyApp extends StatelessWidget {
         //   page: () => CommunityScreen(),
         // ),
       ],
-      home: SetMealNameScreen(),
+      home: ChoiceMenuScreen(),
     );
   }
 }
