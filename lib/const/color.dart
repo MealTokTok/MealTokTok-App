8ff146f2ae228a100d35a0d033ab1726fe40c790
ruluralaa <bsy0302@naver.com>
2024-08-03 16:08:45 +0900

    chore: 컨벤션, 컬러 파일 업데이트
    컨벤션 파일과 컬러 파일을 업데이트했습니다.

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
 const Color PRIMARY_COLOR = Color(0xFFFFAA35);
+const Color GRAY0 = Color(0xFFF6F6F6);
+const Color GRAY2 = Color(0xFFD5D5D5);
+const Color GRAY4 = Color(0xFF686868);
