a0cce93b74cfc34d24c5eb409f3fee71c7bffce7
ruluralaa <bsy0302@naver.com>
2024-08-12 15:14:19 +0900

    chore: const 정보를 추가했습니다.
    text style, color, pubspec.yaml 수정 및 이미지 추가를 진행했습니다.

--- lib/const/color.dart
+++ lib/const/color.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
 const Color PRIMARY_COLOR = Color(0xFFFFAA35);
-const Color GRAY0 = Color(0xFFF6F6F6);
-const Color GRAY2 = Color(0xFFD5D5D5);
-const Color GRAY4 = Color(0xFF686868);
+const Color SECONDARY= Color(0xFFFF714E);
+const Color GRAY0= Color(0xFFF6F6F6);
+const Color GRAY1= Color(0xFFE9E9E9);
+const Color GRAY2= Color(0xFFD5D5D5);
+const Color GRAY3= Color(0xFF999999);
+const Color GRAY4= Color(0xFF686868);
+
+
--- lib/const/style.dart
+++ lib/const/style.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hankkitoktok/const/color.dart';
 
 
 //------------------------------ 로그인 ------------------------------
@@ -20,6 +21,87 @@ TextStyle titleStyle = const TextStyle(
 
 
 //------------------------------ 마이 페이지 ------------------------------
+TextStyle myPageDfault = const TextStyle(
+color: Colors.black,
+fontSize: 16,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w400,
+letterSpacing: -0.32,
+);
+
+TextStyle myPageRecordGrayNum = const TextStyle(
+color: GRAY2,
+fontSize: 18,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w600,
+letterSpacing: -0.36,
+);
+
+TextStyle myPageRecordGrayText = const TextStyle(
+color: GRAY2,
+fontSize: 12,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w400,
+letterSpacing: -0.24,
+);
+
+
+TextStyle myPageRecordGray14 = const TextStyle(
+color: GRAY4,
+fontSize: 14,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w400,
+letterSpacing: -0.28,
+);
+
+TextStyle myPageRecordGray2_14_400 = const TextStyle(
+  color: GRAY2,
+  fontSize: 14,
+  fontFamily: 'Pretendard Variable',
+  fontWeight: FontWeight.w400,
+  letterSpacing: -0.28,
+);
+
+TextStyle myPageRecordGray14500 = const TextStyle(
+color: GRAY3,
+fontSize: 14,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w500,
+letterSpacing: -0.28,
+);
+
+TextStyle myPageRecordGray414500 = const TextStyle(
+  color: GRAY4,
+  fontSize: 14,
+  fontFamily: 'Pretendard Variable',
+  fontWeight: FontWeight.w500,
+  letterSpacing: -0.28,
+);
+
+
+TextStyle myPageRecordBlack16 = const TextStyle(
+color: Colors.black,
+fontSize: 16,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w600,
+letterSpacing: -0.32,
+);
+
+TextStyle myPageRecordBlack14 = const TextStyle(
+  color: Colors.black,
+  fontSize: 14,
+  fontFamily: 'Pretendard Variable',
+  fontWeight: FontWeight.w500,
+  letterSpacing: -0.28,
+);
+
+TextStyle myPageRecordWhite14 = const TextStyle(
+color: Colors.white,
+fontSize: 14,
+fontFamily: 'Pretendard Variable',
+fontWeight: FontWeight.w500,
+letterSpacing: -0.28,
+);
 
 //------------------------------ 마이 페이지 ------------------------------
 
