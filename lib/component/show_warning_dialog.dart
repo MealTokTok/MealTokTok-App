a029b2a2a35b65b896c74800290aa3be4c44d82b
ruluralaa <bsy0302@naver.com>
2024-08-29 16:14:21 +0900

    feat: warningDialog 컴포넌트 생성
    제목 내용 액션 버튼에 들어갈 글자를 입력하면 불러올 수 있습니다.

---
+++ lib/component/show_warning_dialog.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:hankkitoktok/const/color.dart';
+import 'package:hankkitoktok/const/style2.dart';
+
+void showWarningDialog(BuildContext context, String title, String content, String leftbutton, String rightbutton) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24.0), // 둥근 모서리
+        ),
+        contentPadding: EdgeInsets.all(20.0), // 내부 여백
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${title}',
+              //'풀대접 서비스를\n받지 않으실건가요?',
+              style: TextStyles.getTextStyle(TextType.TITLE_2,Colors.black),
+            ),
+
+            Text(
+              '${content}',
+              //'돌아가기를 누르시면 풀대접 서비스는\n장바구니에 담기지 않아요.',
+              style: TextStyles.getTextStyle(TextType.BODY_2,GRAY4),
+            ),
+            // SizedBox(height: 20),
+          ],
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              '${leftbutton}',
+              //'돌아가기',
+              style: TextStyles.getTextStyle(TextType.BUTTON,GRAY4),
+            ),
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.all(12),
+              backgroundColor: GRAY1, // 배경색
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0), // 둥근 모서리
+              ),
+              minimumSize: Size(124, 48),
+            ),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              '${rightbutton}',
+              //'그만두기',
+              style: TextStyles.getTextStyle(TextType.BUTTON,Colors.white),
+            ),
+            style: TextButton.styleFrom(
+              padding: EdgeInsets.all(12),
+              backgroundColor: PRIMARY_COLOR, // 주황색 배경
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0), // 둥근 모서리
+              ),
+              minimumSize: Size(124, 48),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
